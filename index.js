6e99c28af487ef683600d052525fbb1dfe166c4f
unknown <malfellows13@gmail.com>
2022-02-22 19:16:05 -0600

    edited the dogs

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footerText = document.querySelector('.footer')
+footerText.style.color = 'blue'
+
+let dogFlip = document.querySelectorAll('.dog-image')
+for(let i = 0; i <dogFlip.length; i++) {
+    dogFlip[i].style.transform = 'rotate(180deg)'
+}
